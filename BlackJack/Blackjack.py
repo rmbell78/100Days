aae97232fb99d5010b229f7b36dd09eee1a848fb
rmbell78 <rmbell78@gmail.com>
2021-04-11 17:36:31 -0700

    different approach

--- BlackJack/Blackjack.py
+++ BlackJack/Blackjack.py
@@ -1,60 +1,72 @@
 #! /usr/bin/env python3
 import random
-p1cards = []
-p2cards = []
+from art import logo
+import subprocess
 
 def dealer():
     cards = [11,2,3,4,5,6,7,8,9,10,10,10]
-    card = random.choice(cards)
-    return(card)
+    return(random.choice(cards))
 
-def blackjack():
-    p1sum = sum(p1cards)
-    p2sum = sum(p2cards)
-    if p1sum == 21:
-        return(1)
-    elif p2sum == 21:
-        return(2)
+def scorer(cards):
+    if sum(cards) > 21 and 11 in cards:
+        cards.remove(11)
+        cards.append(1)
+        score = sum(cards)
+    elif sum(cards) > 21:
+        score = 0
     else:
-        return(0)
+        score = sum(cards)
+    return(score)
 
-def game():
-    def deal(select):
-        if select == 1:
-            p1cards.append(dealer())
-        if select == 2:
-            p2cards.append(dealer())
-        if select == 3:
-            p1cards.append(dealer())
-            p2cards.append(dealer())  
-    
-    def begin():
-        deal(3)
-        deal(3)
-        if blackjack() == 1:
-            print('You got Blackjack!')
+def compare(p1cards, p2cards):
+    p1score = scorer(p1cards)
+    p2score = scorer(p2cards)
+    if p1score == 0:
+        print('You Bust!')
+    elif p2score == 0:
+        print('Dealer Bust, You Win!')
+    elif p1score == 21 or p2score == 21:
+        if p1score ==21 and p2score == 21:
+            print('You Both got Blackjack, Tie')
+        if p1score == 21:
+            print('You Got Blackjack!')
+        if p2score == 21:
+            print('Dealer Got Blackjack!')
+    elif p2score > p1score:
+        print('Dealer wins with a higher score!')
+    elif p1score == p2score:
+        print('Tie!')
+
+def Play():
+    print(logo)
+    p1cards = []
+    p2cards = []
+    gameover = False
+    for i in range(2):
+        p1cards.append(dealer())
+        p2cards.append(dealer())
+    while gameover == False:
+        p1score = scorer(p1cards)
+        p2score = scorer(p2cards)
+        print(f'    Your Cards: ' + str(p1cards) + ' Your Score: ' + str(p1score))
+        print(f'    Dealers First Card: ' + str(p2cards[0]))
+        if p1score == 0 or p2score == 0 or p1score == 21:
+            gameover = True
         else:
-            print('Your cards: ' + str(p1cards))
-            print('Your score: ' + str(sum(p1cards)))
-            print('Dealers Hand: ' + str(p2cards[0]))
-            userin = input("Y for hit N for stay: ")
-            while userin.upper() == 'Y':
-                deal(1)
-                print('Your cards: ' + str(p1cards))
-                print('Your score: ' + str(sum(p1cards)))
-                userin = input("Another?: ")
-            show()
-    def show():
-        print('Your Final Hand: ' + str(p1cards) + ' , Final Score: ' + str(sum(p1cards)))
-    begin()
-
-
-
-def start():
-    print('Welcome To Blackjack!')
-    userin = input('Want to play a game? (Y/N): ')
-    if userin.upper() == 'Y':
-        game()
-    elif userin.upper() == 'N':
-        print("Why are you even here then?")
-start()
+            Deal = input('Would You Like Another Card? (y/n)')
+            if Deal == 'y':
+                p1cards.append(dealer())
+            else:
+                gameover = True
+    while p2score != 0 and p2score < p1score and p2score != 21:
+        p2cards.append(dealer())
+        p2score = scorer(p2cards)
+    print('Your Final Hand: ' + str(p1cards) + ' Your Final Score: ' + str(p1score))
+    print('Dealers Final Hand ' + str(p2cards) + ' Dealers Final Score: ' + str(p2score))
+    compare(p1cards, p2cards)
+    
+while input('Want to play BlackJack? (y/n): ') == 'y':
+    subprocess.run(['clear'])
+    Play()
+else:
+    print('Why Are You Even Here Then?')
